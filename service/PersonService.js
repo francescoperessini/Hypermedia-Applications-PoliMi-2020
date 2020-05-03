dba2d47e94d1f53aa6c4486f3d96d622d4883b14
tompere <tompere96@gmail.com>
2020-05-03 16:31:22 +0200

    sistemato endpoint people

--- service/PersonService.js
+++ service/PersonService.js
@@ -93,7 +93,73 @@ exports.getUserByID = function(id) {
  **/
 exports.retrieveUsers = function() {
   return new Promise(function(resolve, reject) {
-    resolve();
+    var examples = {};
+    examples['application/json'] = {
+      "surname" : 1,
+      "image_url" : "image_url",
+      "name" : 6,
+      "description" : "description",
+      "id" : 0,
+      "services" : [ {
+        "presentation" : "presentation",
+        "name" : "name",
+        "image_urls" : [ "image_urls", "image_urls" ],
+        "practical_info" : "practical_info",
+        "id" : 5,
+        "events" : [ {
+          "presentation" : "presentation",
+          "image_url" : "image_url",
+          "event_date" : "2000-01-23T04:56:07.000+00:00",
+          "name" : "name",
+          "practical_info" : "practical_info",
+          "id" : 5
+        }, {
+          "presentation" : "presentation",
+          "image_url" : "image_url",
+          "event_date" : "2000-01-23T04:56:07.000+00:00",
+          "name" : "name",
+          "practical_info" : "practical_info",
+          "id" : 5
+        } ]
+      }, {
+        "presentation" : "presentation",
+        "name" : "name",
+        "image_urls" : [ "image_urls", "image_urls" ],
+        "practical_info" : "practical_info",
+        "id" : 5,
+        "events" : [ {
+          "presentation" : "presentation",
+          "image_url" : "image_url",
+          "event_date" : "2000-01-23T04:56:07.000+00:00",
+          "name" : "name",
+          "practical_info" : "practical_info",
+          "id" : 5
+        }, {
+          "presentation" : "presentation",
+          "image_url" : "image_url",
+          "event_date" : "2000-01-23T04:56:07.000+00:00",
+          "name" : "name",
+          "practical_info" : "practical_info",
+          "id" : 5
+        } ]
+      } ],
+      "events" : [ {
+        "presentation" : "presentation",
+        "image_url" : "image_url",
+        "event_date" : "2000-01-23T04:56:07.000+00:00",
+        "name" : "name",
+        "practical_info" : "practical_info",
+        "id" : 5
+      }, {
+        "presentation" : "presentation",
+        "image_url" : "image_url",
+        "event_date" : "2000-01-23T04:56:07.000+00:00",
+        "name" : "name",
+        "practical_info" : "practical_info",
+        "id" : 5
+      } ]
+    };
+    resolve(examples);
   });
 }
 
