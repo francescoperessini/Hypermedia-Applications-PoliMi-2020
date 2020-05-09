f10a2e13d48be2946a74fbe06a54bd4614d45791
Francesco Peressini <francesco.peressini@hotmail.com>
2020-05-09 13:46:20 +0200

    Code correctly formatted

--- public/assets/js/allPerson.js
+++ public/assets/js/allPerson.js
@@ -1,5 +1,5 @@
-getRow = function(person, i){
-    if(i%2 === 0){
+getRow = function (person, i) {
+    if (i % 2 === 0) {
         return '<div class="row mt-5"> <!-- PERSON SECTION -->\n' +
             '        <div class="col-md-1"></div>\n' +
             '        <div class="col-md-9">\n' +
@@ -10,23 +10,22 @@ getRow = function(person, i){
             '                </div>\n' +
             '                <!--USER DESCRIPTION-->\n' +
             '                <div class="col-md-9 align-self-center text-center">\n' +
-            '                    <a href="/pages/person.html?id='+ person.id +'"> <h4>'+ person.name + ' ' + person.surname +'</h4></a>\n' +
-            '                    <p class="mt-4">'+ person.description +'</p>\n' +
+            '                    <a href="/pages/person.html?id=' + person.id + '"> <h4>' + person.name + ' ' + person.surname + '</h4></a>\n' +
+            '                    <p class="mt-4">' + person.description + '</p>\n' +
             '                </div>\n' +
             '            </div>\n' +
             '        </div>\n' +
             '        <div class="col-md-2"></div>\n' +
             '    </div>'
-    }
-    else{
+    } else {
         return '<div class="row mt-5"> <!-- PERSON SECTION -->\n' +
             '        <div class="col-md-2"></div>\n' +
             '        <div class="col-md-9">\n' +
             '            <div class="row badge-light " >\n' +
             '                <!--USER DESCRIPTION-->\n' +
             '                <div class="col-md-9 align-self-center text-center">\n' +
-            '                    <a href="/pages/person.html?id='+ person.id +'"> <h4>'+ person.name + ' ' + person.surname +'</h4></a>\n' +
-            '                    <p class="mt-4">'+ person.description +'</p>\n' +
+            '                    <a href="/pages/person.html?id=' + person.id + '"> <h4>' + person.name + ' ' + person.surname + '</h4></a>\n' +
+            '                    <p class="mt-4">' + person.description + '</p>\n' +
             '                </div>\n' +
             '                <!--USER PHOTO-->\n' +
             '                <div class="col-md-3 text-right">\n' +
@@ -54,7 +53,6 @@ async function getPeople() {
 
     $('#container').append(html)
 }
-
 
 
 //retrieve the author id from URL and fill the page
