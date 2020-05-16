0ace675078890fc1093c3c32d86a0187a73a593b
stefanomartina <stefano.martina@mail.polimi.it>
2020-05-16 16:17:06 +0200

    separator and people page changed

--- public/assets/js/people.js
+++ public/assets/js/people.js
@@ -1,44 +1,21 @@
-getRow = function (person, i) {
-    if (i % 2 === 0) {
-        return '<div class="row mt-5"> <!-- PERSON SECTION -->\n' +
-            '        <div class="col-md-1"></div>\n' +
-            '        <div class="col-md-9">\n' +
-            '            <div class="row badge-light ">\n' +
-            '                <!--USER PHOTO-->\n' +
-            '                <div class="col-md-3 text-center">\n' +
-            '                    <img class="rounded-circle custom-rounded-circle mt-4 mb-4 mr-4" src="../assets/img/person/' + person.image_url + '">\n' +
-            '                </div>\n' +
-            '                <!--USER DESCRIPTION-->\n' +
-            '                <div class="col-md-9 align-self-center text-center">\n' +
-            '                    <a href="/pages/person.html?id=' + person.id + '"> <h4>' + person.name + ' ' + person.surname + '</h4></a>\n' +
-            '                    <p class="mt-4">' + person.description + '</p>\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '        <div class="col-md-2"></div>\n' +
-            '    </div>'
-    } else {
-        return '<div class="row mt-5"> <!-- PERSON SECTION -->\n' +
-            '        <div class="col-md-2"></div>\n' +
-            '        <div class="col-md-9">\n' +
-            '            <div class="row badge-light " >\n' +
-            '                <!--USER DESCRIPTION-->\n' +
-            '                <div class="col-md-9 align-self-center text-center">\n' +
-            '                    <a href="/pages/person.html?id=' + person.id + '"> <h4>' + person.name + ' ' + person.surname + '</h4></a>\n' +
-            '                    <p class="mt-4">' + person.description + '</p>\n' +
-            '                </div>\n' +
-            '                <!--USER PHOTO-->\n' +
-            '                <div class="col-md-3 text-center"">\n' +
-            '                    <img class="rounded-circle custom-rounded-circle mt-4 mb-4 mr-4" src="../assets/img/person/' + person.image_url + '">\n' +
-            '                </div>\n' +
-            '\n' +
-            '            </div>\n' +
-            '        </div>\n' +
-            '        <div class="col-md-1"></div>\n' +
-            '    </div>'
-    }
-
-
+getRow = function (person) {
+    return '<div class="row mt-5"> <!-- PERSON SECTION -->\n' +
+        '        <div class="col-md-1"></div>\n' +
+        '        <div class="col-md-9">\n' +
+        '            <div class="row badge-light ">\n' +
+        '                <!--USER PHOTO-->\n' +
+        '                <div class="col-md-3 text-center">\n' +
+        '                    <img class="rounded-circle custom-rounded-circle mt-4 mb-4 mr-4" src="../assets/img/person/' + person.image_url + '">\n' +
+        '                </div>\n' +
+        '                <!--USER DESCRIPTION-->\n' +
+        '                <div class="col-md-9 align-self-center text-center">\n' +
+        '                    <a href="/pages/person.html?id=' + person.id + '"> <h4>' + person.name + ' ' + person.surname + '</h4></a>\n' +
+        '                    <p class="mt-4">' + person.description + '</p>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '        <div class="col-md-2"></div>\n' +
+        '    </div>'
 }
 
 async function getPeople() {
@@ -48,10 +25,8 @@ async function getPeople() {
         if (response.ok) {
             people = await response.json();
             let html = ""
-            let i = 0
             people.forEach((person) => {
-                html += getRow(person, i)
-                i++
+                html += getRow(person)
             })
             $('#container').append(html)
         } else {
